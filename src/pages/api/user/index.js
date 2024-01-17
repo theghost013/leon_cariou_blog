15f2706614be47a9b3e7eec2d62b162d2d611ec4
Léon <leon.cariou@cyna-it.fr>
2024-01-17 12:40:14 +0100

    user API endpoint

---
+++ src/pages/api/user/index.js
@@ -0,0 +1,52 @@
+import validate from "@/api/middlewares/validate"
+import mw from "@/api/mw"
+import hashPassword from "@/db/hashPassword"
+import { AVERAGE_PASSWORD_HASHING_DURATION } from "@/pages/api/constants"
+import sleep from "@/utils/sleep"
+import {
+  emailValidator,
+  passwordValidator,
+  nameValidator,
+} from "@/utils/validators"
+
+const handle = mw({
+  POST: [
+    validate({
+      body: {
+        firstName: nameValidator.string().required(),
+        lastName: nameValidator.string().required(),
+        email: emailValidator.required(),
+        password: passwordValidator.required(),
+      },
+    }),
+    async ({
+      send,
+      input: {
+        body: { email, password },
+      },
+      models: { UserModel },
+    }) => {
+      const user = await UserModel.query().findOne({ email })
+
+      if (user) {
+        await sleep(AVERAGE_PASSWORD_HASHING_DURATION)
+
+        send(true)
+
+        return
+      }
+
+      const [passwordHash, passwordSalt] = await hashPassword(password)
+
+      await UserModel.query().insert({
+        email,
+        passwordHash,
+        passwordSalt,
+      })
+
+      send(true)
+    },
+  ],
+})
+
+export default handle
