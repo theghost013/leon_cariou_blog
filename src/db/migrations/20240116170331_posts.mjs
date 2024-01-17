b1a76c90c67c195fcc0817ade6d897e30001484f
theghost013 <neolame.junior@gmail.com>
2024-01-17 13:17:26 +0100

    posts migration

--- src/db/migrations/20240116170331_posts.mjs
+++ src/db/migrations/20240116170331_posts.mjs
@@ -1,3 +1,14 @@
-export const up = async (db) => {}
+export const up = async (db) => {
+    await db.schema.createTable("posts", (table) => {
+        table.increments("id").primary()
+        table.text("title").notNullable()
+        table.text("body").notNullable()
+        table.integer("user_id").unsigned().notNullable()
+        table.foreign("user_id").references("users.id")
+        table.timestamps(true, true)
+    })
+}
 
-export const down = async (db) => {}
+export const down = async (db) => {
+    await db.schema.dropTableIfExists("posts")
+}
