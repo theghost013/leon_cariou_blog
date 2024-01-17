e59d57f837b4eeb515da705d879e3498d483e8fa
Léon <leon.cariou@cyna-it.fr>
2024-01-17 13:36:30 +0100

    migration comments

--- src/db/migrations/20240117123351_comments.mjs
+++ src/db/migrations/20240117123351_comments.mjs
@@ -1,14 +1,15 @@
 export const up = async (db) => {
-  await db.schema.createTable("posts", (table) => {
+  await db.schema.createTable("comments", (table) => {
     table.increments("id").primary()
-    table.text("title").notNullable()
     table.text("body").notNullable()
     table.integer("user_id").unsigned().notNullable()
     table.foreign("user_id").references("users.id")
+    table.integer("post_id").unsigned().notNullable()
+    table.foreign("post_id").references("posts.id")
     table.timestamps(true, true)
   })
 }
 
 export const down = async (db) => {
-  await db.schema.dropTableIfExists("posts")
+  await db.schema.dropTableIfExists("comments")
 }
