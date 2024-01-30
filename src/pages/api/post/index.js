2784b45a0999be06e2ac38e738a0950f86f91ae3
Léon <leon.cariou@cyna-it.fr>
2024-01-30 10:27:02 +0100

    Add GET and POST handlers for posts API

--- src/pages/api/post/index.js
+++ src/pages/api/post/index.js
@@ -1,17 +1,35 @@
+import mw from "@/api/mw"
+
 const handle = mw({
   GET: [
-    async ({ send, models: { UserModel } }) => {
-      const users = await UserModel.query().select(
+    async ({ send, models: { PostModel } }) => {
+      const posts = await PostModel.query().select(
         "id",
-        "username",
-        "email",
-        "isActive",
-        "role",
+        "title",
+        "content",
         "created_at",
         "updated_at",
       )
 
-      send(users)
+      send(posts)
+    },
+  ],
+  POST: [
+    async ({
+      send,
+      input: {
+        query: { userId },
+        body: { title, body },
+      },
+      models: { PostModel },
+    }) => {
+      const post = await PostModel.query().insert({
+        title,
+        body,
+        userId,
+      })
+
+      send(post)
     },
   ],
 })
